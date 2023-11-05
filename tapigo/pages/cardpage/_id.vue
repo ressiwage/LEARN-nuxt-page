5bd527029a9c1a642fbfbc8b1f4d54e653bf7330
1 <ressiwage@ya.ru>
2023-11-05 21:38:01 +0500

    4 hours passed

---
+++ tapigo/pages/cardpage/_id.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from '@nuxtjs/composition-api'
+
+
+const route = useRoute()
+const post = ref({})
+fetch('http://127.0.0.1:4000', {
+headers: {
+  'Accept': 'application/json'
+}})
+.then(response => response.json())
+.then(json => {post.value=json[route.value.params.id] ; console.log(post.value)})
+
+</script>
+
+<template>
+  <div>
+    <div class="ms-5">
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+
+    <div class="container">
+      <h1>a user is {{post.name}}</h1>
+      <p>email: {{post.email}}</p>
+      <p>phone: {{post.phone}}</p>
+      <p>address: {{post.address}}</p>
+      <p>position name: {{post.position_name}}</p>
+      <p>department: {{post.department}}</p>
+      <p>hire date: {{post.hire_date}}</p>
+    </div>
+
+  </div>
+</template>
+
+
+
+export default {
+  name: 'CardPage',
+}
---
+++ tapigo/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import CardVue from '../components/Card.vue'
+
+const posts = ref([])
+fetch('http://127.0.0.1:4000', {
+headers: {
+  'Accept': 'application/json'
+}})
+.then(response => response.json())
+.then(json => {posts.value=json })
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+        <div v-for="(card, index) in posts" :key="card.name" class="col-3 h-50">
+            <CardVue :data="card" :url="index"></CardVue>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  
+  name: 'IndexPage',
+}
+</script>
